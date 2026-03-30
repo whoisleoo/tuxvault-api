946139e579dd2db5984c9b77a6e7da59505c3804
Bernardo <leoaaa96@gmail.com>
2026-03-30 15:31:21 -0300

    POST de criação de folder concluido

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -6,12 +6,19 @@ import { db } from '../db/index.js'
 import { eq, isNull } from 'drizzle-orm'
 import { upload } from '../config/multer.js';
 import { requireAuth } from '../middlewares/requireAuth.js';
+import * as path from 'path';
+import { mkdir } from 'fs/promises';
+
 
 
 const uploadBodySchema = z.object({
     parentId: z.string().uuid().optional()
   });
 
+  const folderSchema = z.object({
+    name: z.string().min(1).max(255),
+    parentId: z.string().uuid().optional()
+})
 
 const file: Router = Router();
 
@@ -84,10 +91,103 @@ file.post('/upload', requireAuth, upload.single('file'), async (req: Request, re
         }).returning();
 
 
-        return res.status(200).json({
+        return res.status(201).json({
             newFile
         })
 
+
+
+}catch(err){
+    res.status(500).json({
+        error: "Erro interno do servidor."
+    })
+}
+
+});
+
+
+
+
+
+file.post('/folder', requireAuth, async (req: Request, res: Response) => {
+    try{    
+        const BASE_PATH = '/data/vault'
+
+        const { name, parentId } = folderSchema.parse(req.body);
+
+        // if(!name){
+        //     return res.status(401).json({
+        //         error: "Nome da pasta não informado."
+        //     })
+        // }
+
+        let folderPath: string
+
+
+        if(parentId){
+            const parentFolder = await db.select().from(files).where((eq(files.id, parentId)));
+
+            if(!parentFolder[0]){
+                return res.status(404).json({error: "Pasta de destino não encontrada."})
+            }
+
+            
+            if(!parentFolder[0].isDirectory){
+                return res.status(404).json({error: "O destino não é uma pasta."})
+            }
+
+            const existing = await db.select().from(files).where(eq(files.parentId, parentId));
+
+            const alreadyExists = existing.find(f => f.name === name);
+
+            if(alreadyExists){
+                return res.status(409).json({
+                    error: "Já existe uma pasta com esse nome."
+                })
+            }
+
+
+            folderPath = path.join(parentFolder[0].path, name);
+
+    
+
+            await mkdir(folderPath, { recursive: true});
+
+            
+        }else{
+            folderPath = path.join(BASE_PATH, name);
+        }
+
+        await mkdir(folderPath, { recursive: true});
+
+
+         
+
+
+
+
+        const [newFolder] = await db.insert(files).values({
+            name: name,
+            path: folderPath,
+            parentId: parentId ?? null,
+            isDirectory: true,
+            ownerUsername: req.session.username!
+
+        }).returning();
+
+
+        return res.status(201).json({
+            newFolder
+        })
+
+
+
+
+
+
+
+
+     
 
 
 }catch(err){
